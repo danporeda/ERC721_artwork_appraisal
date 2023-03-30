e80cf6cd13912f86dc7a40c0980f118fbab9edb1
Dan <danporeda@yahoo.com>
2023-03-30 14:49:54 -0400

    initial push

---
+++ contracts/artwork.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.7 <0.9;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
+
+contract ArtToken is ERC721Full {
+    constructor() public ERC721Full("ArtToken", "ART") {}
+
+    function registerArtwork(address owner, string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        uint256 tokenId = totalSupply();
+        _mint(owner, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+
+        return tokenId;
+    }
+}
